94cb8e521c5755b367fa48656c696c6a1cf883f2
wangxinkuan <wangxinkuan@mmears.com>
2019-04-22 17:52:40 +0800

    添加对video的支持

--- static/js/shakeda.js
+++ static/js/shakeda.js
@@ -1,3 +1,6 @@
 // img 包裹 div 并设置overflow
 $('div.content_placeholder-div img').wrap('<div></div>')
 $('div.content_placeholder-div img:not(.artical-logo)').parent().css('overflow', 'auto');
+
+$('div.content_placeholder-div video').wrap('<div></div>')
+$('div.content_placeholder-div video').parent().css('overflow', 'auto').css("radius","20px");
